77e398064f2e54593a3bdb320143a2e5bd928072
Michael Ackstaller <m.ackstaller@outlook.de>
2024-06-11 20:19:16 +0200

    added first Methods of REST API

--- lib/data/models/firm_element.dart
+++ lib/data/models/firm_element.dart
@@ -10,11 +10,38 @@ class FirmElement {
   final ElementType type;
   final FirmGroup firmGroup;
 
-  FirmElement(
-      {required this.id,
-      required this.name,
-      required this.should_done,
-      required this.is_done,
-      required this.type,
-      required this.firmGroup});
+  FirmElement({
+    required this.id,
+    required this.name,
+    required this.should_done,
+    required this.is_done,
+    required this.type,
+    required this.firmGroup,
+  });
+
+  factory FirmElement.fromJson(Map<String, dynamic> json) {
+    return FirmElement(
+      id: json['id'],
+      name: json['name'],
+      should_done: json['should_done'] != null
+          ? DateTime.parse(json['should_done'])
+          : null,
+      is_done: json['is_done'] != null ? DateTime.parse(json['is_done']) : null,
+      type: ElementType.values
+          .firstWhere((e) => e.toString() == 'ElementType.${json['type']}'),
+      firmGroup: FirmGroup.values
+          .firstWhere((e) => e.toString() == 'FirmGroup.${json['firmGroup']}'),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'should_done': should_done?.toIso8601String(),
+      'is_done': is_done?.toIso8601String(),
+      'type': type.toString().split('.').last,
+      'firmGroup': firmGroup.toString().split('.').last,
+    };
+  }
 }
--- lib/data/models/firm_user.dart
+++ lib/data/models/firm_user.dart
@@ -5,4 +5,18 @@ class FirmUser {
   final String name;
 
   FirmUser({required this.id, required this.name});
+
+  factory FirmUser.fromJson(Map<String, dynamic> json) {
+    return FirmUser(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
 }

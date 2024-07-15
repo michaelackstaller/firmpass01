a169abe21ffe0e46a72c85d1662f8288cad3d1c7
Dominik Wildgruber <dominik.wildgruber@gmail.com>
2024-07-15 18:45:14 +0200

    kleiner bugfix

--- lib/screens/manualSearch_Page.dart
+++ lib/screens/manualSearch_Page.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:firmpass/api/Api.dart';
+import 'package:firmpass/api/api.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
